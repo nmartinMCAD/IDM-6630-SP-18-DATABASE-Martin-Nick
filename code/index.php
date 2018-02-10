6ee4a546dd7a384a6285d6906ac81353f81ad345
nmartinMCAD <nmartin@mcad.edu>
2018-02-10 15:53:04 -0700

    WEEK 4 FINAL
    Annnnd Done.

--- code/index.php
+++ code/index.php
@@ -3,20 +3,29 @@
   <head>
     <meta charset="utf-8">
     <title>Nick's Picks: Albums</title>
-    <link href="https://fonts.googleapis.com/css?family=Catamaran:300,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:400,900" rel="stylesheet">
     <link rel="stylesheet" href="/css/css.css">
   </head>
   <body>
 
+    <header>
+
+      <?php
+
+      echo "<h1>Nick's Picks</h1>";
+      echo "<h2>Top 10 Albums</h2>";
+
+      ?>
+
+    </header>
+
+    <main>
     <?php
 
       $host = '172.25.0.2';
       $database = 'week03';
       $user = 'idm6630';
       $pass = 'idm6630';
-
-      echo "<h1>Nick's Picks</h1>";
-      echo "<h2>Top 10 Albums</h2>";
 
       try {
 
@@ -33,19 +42,21 @@
 
       $query = 'SELECT * FROM albums';
 
-      echo "<div>";
-
       foreach ($conn->query($query) as $album) {
+        echo "<div>";
         echo "<h3>" . $album['ID'] . "</h3>";
         echo "<h4>" . $album['artist'] . "</h4>";
         echo "<h5>" . $album['title'] . "</h5>";
         echo "<h6>" . $album['year'] . "</h6>";
         echo "<img src='" . $album['cover'] . "'>";
-      }
-
-      echo "</div>";
+        echo "</div>";
+      };
 
     ?>
+    </main>
 
+    <footer>
+      <a href="#">Back to Top</a>
+    </footer>
   </body>
 </html>
--- data/data.sql
+++ data/data.sql
@@ -37,7 +37,7 @@ CREATE TABLE `albums` (
   `year` varchar(255) NOT NULL,
   `cover` varchar(255) NOT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -59,4 +59,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-02-10 21:30:22
+-- Dump completed on 2018-02-10 22:52:30
